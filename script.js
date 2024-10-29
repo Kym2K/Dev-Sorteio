9d5165efd73fa855b48b75bb98c93ef306717ee7
Kym2K <kim2ksergio@gmail.com>
2024-10-29 20:52:39 -0300

    Adição JS

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function generateNumber(){
-    
+function generateNumber() {
+
     const min = Math.ceil(document.querySelector(".input-2").value)
     const max = Math.floor(document.querySelector(".input").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1) ) + min;
+    if (min >= max) {
 
+       alert("o valor minimo não pode ser maior que o maximo")
+    }
+    
+    else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
+  
     alert(result)
+    }
 }
